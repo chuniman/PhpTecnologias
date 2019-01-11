0998cb2528e16a9f85fd6d54618b74db926e5675
Juan Ignacio Zunino <zunino.juanignacio@gmail.com>
2019-01-11 17:57:45 -0300

    ahora el twig anda, y tambien hay validaciones y se suben archivos. se van a poner imagenes al job

--- app/controllers/JobsController.php
+++ app/controllers/JobsController.php
@@ -3,24 +3,49 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use Respect\Validation\Validator as v;
 
-class JobsController extends BaseController{
+class JobsController extends BaseController {
 
     public function AddJobAction($request) {
-        if ($request->getMethod()=='POST') {
-            
-            $postData=$request->getParsedBody();
-            $job = new Job();
+        $responseMessage = null;
+        if ($request->getMethod() == 'POST') {
 
-            $job->title = $postData['title-jobs'];
-            $job->description = $postData['description-jobs'];
-            $job->visible = $postData['visible-jobs'];
-            $job->months = $postData['months-jobs'];
-            
-            $job->save();
+            $postData = $request->getParsedBody();
+
+            $jobValidator = v::key('title-jobs', v::stringType()->notEmpty())->key('description-jobs', v::stringType()->notEmpty());
+
+            try {
+                //validador
+                $jobValidator->assert($postData);
+                //seccion job
+                $job = new Job();
+
+                $job->title = $postData['title-jobs'];
+                $job->description = $postData['description-jobs'];
+                $job->visible = $postData['visible-jobs'];
+                $job->months = $postData['months-jobs'];
+                
+                //seccion archivo esta es la parte del archivo, anda por ahora te deja la imagen en la carpeta uploads
+                //no es lo recomendable pero es un ejemplo de algo que anda
+                //estaria para ahora hacer que los jobs tengan imagenes
+//                $files=$request->getUploadedFiles();
+//                var_dump($files);
+//                $imagenNar=$files['file-jobs'];
+//                
+//                if ($imagenNar->getError()== \UPLOAD_ERR_OK){
+//                    $fileName=$imagenNar->getClientFilename();
+//                    $imagenNar->moveTo("uploads/$fileName");
+//                }
+
+                $job->save();
+                $responseMessage = 'Saved';
+            } catch (\Exception $exc) {
+                $responseMessage = $exc->getMessage();
+            }
         }
         //include '../views/addJob.twig';
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig',['responseMessage'=>$responseMessage]);
     }
 
 }
